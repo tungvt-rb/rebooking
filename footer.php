92e22c0ab494aeb1473624806069ae3acb243fab
tungvt-rb <vuthanhtung.rubysoftware@gmail.com>
2017-08-29 23:46:23 +0700

    update footer
    add some content to footer and style for it

--- footer.php
+++ footer.php
@@ -8,7 +8,9 @@
 			<div class="foot-content">
 				<div class="copyright">
 					<p>Copyright © 2017</p>
-					<p>Add: 101 Buckland, NZ</p>
+					<p>Add: 28 To Ngoc Van - Tay Ho - Hanoi - Viet Nam</p>
+					<p>Tel: + 8443678 5678 MB: + 84 90 888 9818</p>
+					<p>Email: rebooking@mail.com</p>
 				</div>
 			</div><!-- .foot-content -->
 		</div>		

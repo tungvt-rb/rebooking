7ef5a6da21a6d8efb29dd501e4be1e5638ed0b4f
tungvt-rb <vuthanhtung.rubysoftware@gmail.com>
2017-08-31 16:52:01 +0700

    add string language
    add string language

--- inc/lib/lib.translate.php
+++ inc/lib/lib.translate.php
@@ -2,9 +2,15 @@
 
 pll_register_string('currency', '$');
 
-pll_register_string('property_details', 'Price');
-pll_register_string('property_details', 'Location');
-pll_register_string('property_details', 'Bedroom');
+pll_register_string('Price', 'Price', 'property_details');
+pll_register_string('Location', 'Location', 'property_details');
+pll_register_string('Bedroom', 'Bedroom', 'property_details');
+pll_register_string('Bathroom', 'Bathroom', 'property_details');
+pll_register_string('Bathroom Type', 'Bathroom Type', 'property_details');
+pll_register_string('Living area', 'Living area', 'property_details');
+pll_register_string('Built in', 'Built in', 'property_details');
+pll_register_string('Furnished', 'Furnished', 'property_details');
+pll_register_string('Equipped', 'Equipped', 'property_details');
 
-pll_register_string('page_content', 'SEARCH BY TYPE & LOCATION');
-pll_register_string('page_content', 'No post found!');
+pll_register_string('Page title', 'SEARCH BY TYPE & LOCATION', 'page_content');
+pll_register_string('Message info', 'No post found!', 'page_content');

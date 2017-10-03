f0094793f032dec7635042abe24e6582fb906b28
tungvt-rb <vuthanhtung.rubysoftware@gmail.com>
2017-10-03 10:59:34 +0700

    create new js file for singular page
    add new js for singular page

---
+++ assets/js/singular.js
@@ -0,0 +1,11 @@
+(function(jQSingular){
+	jQSingular(function(){
+		jQSingular('#phone').mask('(999) 999-9999');
+
+		jQSingular('.bxslider').bxSlider({
+			mode: 'fade',
+			captions: true,
+			auto: true,
+		});
+	});
+})(jQuery);
--- inc/classes/functions/function.scripts.php
+++ inc/classes/functions/function.scripts.php
@@ -10,11 +10,18 @@ $jsObject = array(
 );
 
 wp_localize_script('jquery', 'reb', $jsObject);
-
-wp_enqueue_script('bxslider', $this->templateURL . '/assets/js/jquery.bxslider.min.js', array('jquery'), false, true);
+//common js
 wp_enqueue_script('simpleWeather', $this->templateURL . '/assets/js/jquery.simpleWeather.min.js', array('jquery'), false, true);
-wp_enqueue_script('fn', $this->templateURL . '/assets/js/fn.js', array('jquery'), false, true);
 
+//singular
 if ( $jsObject['is_singular'] ) {
+	//js
+	wp_enqueue_script('bxslider', $this->templateURL . '/assets/js/jquery.bxslider.min.js', array('jquery'), false, true);
+	wp_enqueue_script('maskedinput', $this->templateURL . '/assets/js/jquery.maskedinput.min.js', array('jquery'), false, true);
+	wp_enqueue_script('singular', $this->templateURL . '/assets/js/singular.js', array('jquery'), false, true);
+	//css
 	wp_enqueue_style( 'style-bxslider', $this->templateURL . '/assets/css/jquery.bxslider.min.css');
 }
+
+// fn.js
+wp_enqueue_script('fn', $this->templateURL . '/assets/js/fn.js', array('jquery'), false, true);

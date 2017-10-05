4b309dfbc57e01256d93711e3ceb43003e6f32e3
tungvt-rb <vuthanhtung.rubysoftware@gmail.com>
2017-10-05 15:10:09 +0700

    fix error deactive polylang
    add check function exists pll_register_string when deactive polylang

--- inc/lib/lib.translate.php
+++ inc/lib/lib.translate.php
@@ -1,35 +1,38 @@
 <?php
 
-pll_register_string('currency', '$');
+if ( function_exists( 'pll_register_string' ) ) {
 
-pll_register_string('Price', 'Price', 'property_details');
-pll_register_string('Location', 'Location', 'property_details');
-pll_register_string('Bedroom', 'Bedroom', 'property_details');
-pll_register_string('Bathroom', 'Bathroom', 'property_details');
-pll_register_string('Bathroom Type', 'Bathroom Type', 'property_details');
-pll_register_string('Living area', 'Living area', 'property_details');
-pll_register_string('Built in', 'Built in', 'property_details');
-pll_register_string('Furnished', 'Furnished', 'property_details');
-pll_register_string('Equipped', 'Equipped', 'property_details');
-pll_register_string('Free services', 'Free services', 'property_details');
-pll_register_string('Pay Services', 'Pay Services', 'property_details');
-pll_register_string('Environment', 'Environment', 'property_details');
-pll_register_string('Furniture', 'Furniture', 'property_details');
-pll_register_string('Details', 'Details', 'property_details');
+	pll_register_string('currency', '$');
 
-pll_register_string('Label H3(details header) in single property', 'Details of apartment for rent', 'property_details');
-pll_register_string('Label "Property ID" in single property', 'Property ID', 'property_details');
-pll_register_string('Label after property ID in single property', 'Please use this', 'property_details');
-pll_register_string('$/month', 'month', 'property_details');
+	pll_register_string('Price', 'Price', 'property_details');
+	pll_register_string('Location', 'Location', 'property_details');
+	pll_register_string('Bedroom', 'Bedroom', 'property_details');
+	pll_register_string('Bathroom', 'Bathroom', 'property_details');
+	pll_register_string('Bathroom Type', 'Bathroom Type', 'property_details');
+	pll_register_string('Living area', 'Living area', 'property_details');
+	pll_register_string('Built in', 'Built in', 'property_details');
+	pll_register_string('Furnished', 'Furnished', 'property_details');
+	pll_register_string('Equipped', 'Equipped', 'property_details');
+	pll_register_string('Free services', 'Free services', 'property_details');
+	pll_register_string('Pay Services', 'Pay Services', 'property_details');
+	pll_register_string('Environment', 'Environment', 'property_details');
+	pll_register_string('Furniture', 'Furniture', 'property_details');
+	pll_register_string('Details', 'Details', 'property_details');
 
-pll_register_string('Label in google map', 'PROPERTY LOCATION', 'property_details');
+	pll_register_string('Label H3(details header) in single property', 'Details of apartment for rent', 'property_details');
+	pll_register_string('Label "Property ID" in single property', 'Property ID', 'property_details');
+	pll_register_string('Label after property ID in single property', 'Please use this', 'property_details');
+	pll_register_string('$/month', 'month', 'property_details');
 
-pll_register_string('Page title', 'SEARCH BY TYPE & LOCATION', 'page_content');
-pll_register_string('Message info', 'No post found!', 'page_content');
-pll_register_string('Message info', 'No contact!', 'page_content');
-pll_register_string('Message info', 'No company selected!', 'page_content');
-pll_register_string('PROPERTY LIST', 'PROPERTY LIST', 'page_content');
+	pll_register_string('Label in google map', 'PROPERTY LOCATION', 'property_details');
 
-pll_register_string('Label control (List property)', 'View detail', 'controls');
-pll_register_string('Label control (Search form)', 'Search', 'controls');
+	pll_register_string('Page title', 'SEARCH BY TYPE & LOCATION', 'page_content');
+	pll_register_string('Message info', 'No post found!', 'page_content');
+	pll_register_string('Message info', 'No contact!', 'page_content');
+	pll_register_string('Message info', 'No company selected!', 'page_content');
+	pll_register_string('PROPERTY LIST', 'PROPERTY LIST', 'page_content');
 
+	pll_register_string('Label control (List property)', 'View detail', 'controls');
+	pll_register_string('Label control (Search form)', 'Search', 'controls');
+	
+}
